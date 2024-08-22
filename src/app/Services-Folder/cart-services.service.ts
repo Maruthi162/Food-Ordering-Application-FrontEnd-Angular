ba90b683f882a826163f3ac6f1d65610e9a1e43a
Maruthi N <nmaruthi162@gmail.com>
2024-08-22 09:07:26 +0530

    Fixed Cart count issue

--- src/app/Services-Folder/cart-services.service.ts
+++ src/app/Services-Folder/cart-services.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable } from 'rxjs';
 import { cartDto } from '../Models/cartDto';
 
 @Injectable({
@@ -9,10 +9,21 @@ import { cartDto } from '../Models/cartDto';
 export class CartServicesService {
 
   apiUrl="https://localhost:44351/api/Cart";
+  cartCount: BehaviorSubject<number> = new BehaviorSubject<number>(0); // Reactive cart count
   constructor(private http:HttpClient) { }
 
   getCartItems(userId:string):Observable<any>{
-    return this.http.get<any>(`${this.apiUrl}/cart?userId=${userId}`);
+    return this.http.get<any>(`${this.apiUrl}/cart?userId=${userId}`).pipe(
+      map((response:any[])=>{
+        this.cartCount.next(response.length); // Update cart count
+        return response;
+      })
+    );
+
+  }
+
+  getCartCount(): Observable<number> {
+    return this.cartCount.asObservable(); // Return the observable of cart count
   }
 
   removeFromCart(cartItemId:number):Observable<any>{
